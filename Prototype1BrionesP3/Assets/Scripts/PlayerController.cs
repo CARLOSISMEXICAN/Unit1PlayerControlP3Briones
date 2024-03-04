851cb3a74b409e5b3519932c325c57bd49a924c7
Carlos <722990@student.dusd.net>
2024-03-04 11:42:05 -0800

    added more obstacle, fixed the turn, and organized code.
    I made a pyramid out of obstacles and I made the vehicle turn better than before and I organized my hierarchy and code.

--- Prototype1BrionesP3/Assets/Scripts/PlayerController.cs
+++ Prototype1BrionesP3/Assets/Scripts/PlayerController.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5.0f;
-    public float turnSpeed;
-    public float horizontalInput;
+    // Private Variables
+    private float speed = 5.0f;
+    private float turnSpeed = 100.0f;
+    private float horizontalInput;
+    private float forwardInput;
 
     // Start is called before the first frame update
     void Start()
@@ -17,9 +19,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal")
+        // This is where we get player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
         // Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // We turn the vehicle
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
